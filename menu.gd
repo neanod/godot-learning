9404fac0e7fc78d035a5176735a7c9ffd23698ec
Kosta pid <markovsemen155@gmail.com>
2024-04-13 15:17:13 +0300

    design

--- menu.gd
+++ menu.gd
@@ -8,3 +8,8 @@ func _on_button_2_pressed():
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://levels/lobby.tscn")
+
+
+func _on_button_3_pressed():
+	get_tree().change_scene_to_file("res://autors.tscn"
+	)
